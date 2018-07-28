bbd0bafdbf67ccf063c34c110dc0751557669dde
Evan Tucker <eej.tucker@gmail.com>
2018-07-27 21:29:03 -0600

    worked on SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Form, FormGroup, Label, Input} from 'reactstrap';
 
-const SearchBar = () => {
-    return (
-        <h2>Search bar</h2>
-    );
+class SearchBar extends Component {
+
+    state = {
+        term: ''
+    }
+
+    handleInputChange = () => {
+
+    }
+
+    render() {
+        return (
+            <Form>
+                <FormGroup>
+                    <Label for="term" hidden>Email</Label>
+                    <Input 
+                        type="term" 
+                        name="term" 
+                        id="term" 
+                        placeholder="Search for something!"
+                    />
+                </FormGroup>
+            </Form>
+        );
+    }
 }
 
 export default SearchBar;
