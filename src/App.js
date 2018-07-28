27f458fa3dede6a38f13795ea7c23ce0d7e48e9c
Evan Tucker <eej.tucker@gmail.com>
2018-07-27 19:47:21 -0600

    added reactstrap and basic grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import SearchBar from './components/SearchBar';
+import { Container, Row, Col } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <SearchBar/>
-      </div>
+      <Container>
+          <Row>
+            <Col>
+              <SearchBar/>
+            </Col>
+          </Row>
+          <Row>
+            <Col md="8" sm="12">
+              Video Detail
+            </Col>
+            <Col md="4" sm="12">
+              Video List
+            </Col>
+          </Row>
+      </Container>
     );
   }
 }
